b7f74fbd03fbf2a8a132159ccbc8644d043695fd
enock <enockmambou@gmail.com>
2025-08-27 22:59:45 +0000

    update package install express

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  server: { host: true }
+  server: { host: true },
+    preview: {
+        port: 3000,
+        host: true, // écoute sur toutes les IP
+        allowedHosts: ['join.supdeco.sn'] // ton domaine autorisé
+    }
 })
