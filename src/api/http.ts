ffbf23080969a7a6f4e6e5e57111107cfa2e78c1
enock <enockmambou@gmail.com>
2025-08-27 23:51:48 +0000

    prod axios

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,7 +1,15 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api'
+// Définir l'URL de base selon l'environnement
+let baseURL = ''
 
+if (import.meta.env.MODE === 'production') {
+    // En prod → utiliser l'API prod
+    baseURL = import.meta.env.VITE_API_BASE_URL_PROD || 'https://admissions.supdeco.sn/api'
+} else {
+    // En dev → utiliser localhost
+    baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api'
+}
 export const http = axios.create({
   baseURL,
   headers: { 'Content-Type': 'application/json' },
